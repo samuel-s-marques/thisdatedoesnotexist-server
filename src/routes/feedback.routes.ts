1b5e5df748b0d4a7c9ab9494648d4fee999c29f5
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-13 19:46:02 -0300

    add multer

--- src/routes/feedback.routes.ts
+++ src/routes/feedback.routes.ts
@@ -1,21 +1,37 @@
 import { Router } from "express";
 import FeedbackController from "../controllers/feedback.controller.js";
+import { v4 as uuidv4 } from "uuid";
 
 class FeedbackRoutes {
-    router = Router();
-    controller = new FeedbackController();
+  router = Router();
+  controller = new FeedbackController();
+  multer = require("multer");
+  imageStorage = this.multer.diskStorage({
+    destination: "output/images/",
+    filename: (req: any, file: any, cb: any) => {
+      const uuid = uuidv4();
+      cb(null, `${uuid}.png`);
+    },
+  });
+  upload = this.multer({
+    storage: this.imageStorage,
+  });
 
-    constructor() {
-        this.initializeRoutes();
-    }
+  constructor() {
+    this.initializeRoutes();
+  }
 
-    initializeRoutes() {
-        this.router.post("/", this.controller.create);
-        this.router.get("/", this.controller.getAll);
-        this.router.get("/:id", this.controller.get);
-        this.router.put("/:id", this.controller.update);
-        this.router.delete("/:id", this.controller.delete);
-    }
+  initializeRoutes() {
+    this.router.post(
+      "/",
+      this.upload.single("screenshot"),
+      this.controller.create
+    );
+    this.router.get("/", this.controller.getAll);
+    this.router.get("/:id", this.controller.get);
+    this.router.put("/:id", this.controller.update);
+    this.router.delete("/:id", this.controller.delete);
+  }
 }
 
 export default new FeedbackRoutes().router;
