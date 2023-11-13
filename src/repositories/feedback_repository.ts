58be03881610d742eed2af5afb78895af5ca4561
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-13 16:37:42 -0300

    implement `createFeedback`

--- src/repositories/feedback_repository.ts
+++ src/repositories/feedback_repository.ts
@@ -1,4 +1,6 @@
 import { IFeedback } from "../models/feedback_model.js";
+import connection from "../config/db.config.js";
+import { ResultSetHeader } from "mysql2";
 
 interface IFeedbackRepository {
   getFeedbacks(): Promise<IFeedback[]>;
@@ -18,7 +20,21 @@ class FeedbackRepository implements IFeedbackRepository {
   }
 
   async createFeedback(feedback: IFeedback): Promise<IFeedback> {
-    throw new Error("Method not implemented.");
+    return new Promise((resolve, reject) => {
+      connection.query<ResultSetHeader>(
+        "INSERT INTO feedback(user_uid, text, screenshot) VALUES(?, ?, ?)",
+        [feedback.user_uid, feedback.text, feedback.screenshot],
+        (err, res) => {
+          if (err) {
+            reject(err);
+          } else {
+            this.getFeedback(res.insertId)
+              .then((feedback) => resolve(feedback))
+              .catch(reject);
+          }
+        }
+      );
+    });
   }
 
   async updateFeedback(id: number, feedback: IFeedback): Promise<IFeedback> {
