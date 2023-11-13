2e007b8a63c9f23252e8d0d3031ff7c246aa1718
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-13 19:45:50 -0300

    add image

--- src/controllers/feedback.controller.ts
+++ src/controllers/feedback.controller.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
 import FeedbackRepository from "../repositories/feedback.repository";
 import { IFeedback } from "../models/feedback_model";
-import { processImage } from "../util/utils";
 
 export default class FeedbackController {
   async create(req: Request, res: Response) {
-    if (!req.body.user_uid || !req.body.text || !req.body.screenshot) {
+    if (
+      !req.body.user_uid ||
+      !req.body.text ||
+      !req.file ||
+      !req.file.originalname.endsWith(".png")
+    ) {
       res.status(400).send({
         status: "error",
         message:
@@ -15,8 +19,16 @@ export default class FeedbackController {
       return;
     }
 
+    const image = req.file.filename.split(".")[0];
+
     try {
-      let feedback: IFeedback = req.body;
+      let feedback: IFeedback = {
+        user_uid: req.body.user_uid,
+        text: req.body.text,
+        screenshot: image,
+        created_at: undefined,
+        constructor: { name: "RowDataPacket" },
+      };
       const createdFeedback = await FeedbackRepository.createFeedback(feedback);
 
       res.status(201).send(createdFeedback);
