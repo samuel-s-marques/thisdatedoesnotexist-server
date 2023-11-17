21298cdc85ddd1fad32f83dcbe7d9736b6dfe92a
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-17 15:07:57 -0300

    call character routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Application } from "express";
 import feedbackRoutes from "./feedback.routes.js";
+import characterRoutes from "./character.routes.js";
 
 export default class Routes {
-    constructor(app: Application) {
-        app.use('/api/feedback', feedbackRoutes);
-    }
+  constructor(app: Application) {
+    app.use("/api/feedback", feedbackRoutes);
+    app.use("/api/character", characterRoutes);
+  }
 }
