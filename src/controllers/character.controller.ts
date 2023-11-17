171d02e53bc786955fe7b3ea9dcc79d136b023f5
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-17 19:05:31 -0300

    create character with associations

--- src/controllers/character.controller.ts
+++ src/controllers/character.controller.ts
@@ -4,6 +4,10 @@ import { Request, Response } from "express";
 import { ICharacter } from "../models/character.model";
 import { v4 as uuidv4 } from "uuid";
 import characterRepository from "../repositories/character.repository";
+import personalityTraitRepository from "../repositories/personality_trait.repository";
+import hobbyRepository from "../repositories/hobby.repository";
+import characterPersonalityTraitRepository from "../repositories/character_personality_trait.repository";
+import characterHobbyRepository from "../repositories/character_hobby.repository";
 
 export default class CharacterController {
   cronJob: CronJob;
@@ -26,6 +30,9 @@ export default class CharacterController {
   async create() {
     const forge: CharacterForge = new CharacterForge();
     const forgedCharacter: Character = forge.forge();
+    const forgedPersonalityTraits: string[] =
+      forgedCharacter.personalityTraits.map((trait) => trait.name);
+    const forgedHobbies: string[] = forgedCharacter.hobbies;
     const character: ICharacter = {
       uuid: uuidv4(),
       ...forgedCharacter,
@@ -33,7 +40,34 @@ export default class CharacterController {
       constructor: { name: "RowDataPacket" },
     };
 
-    await characterRepository.createCharacter(character);
+    const savedCharacter: ICharacter =
+      await characterRepository.createCharacter(character);
+
+    const personalityTraits =
+      await personalityTraitRepository.getPersonalityTraitsByNameArray(
+        forgedPersonalityTraits
+      );
+    const hobbies = await hobbyRepository.getHobbiesByNameArray(forgedHobbies);
+
+    for (let i = 0; i < personalityTraits.length; i++) {
+      const trait = personalityTraits[i];
+
+      characterPersonalityTraitRepository.createCharacterPersonalityTrait({
+        character_id: savedCharacter.id!,
+        trait_id: trait.id!,
+        constructor: { name: "RowDataPacket" },
+      });
+    }
+
+    for (let i = 0; i < hobbies.length; i++) {
+      const hobby = hobbies[i];
+
+      characterHobbyRepository.createCharacterHobby({
+        character_id: savedCharacter.id!,
+        hobby_id: hobby.id!,
+        constructor: { name: "RowDataPacket" },
+      });
+    }
   }
 
   async delete(req: Request, res: Response) {}
