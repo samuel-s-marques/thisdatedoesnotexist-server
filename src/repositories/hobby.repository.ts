2ad59a414fc465dc2673ee03c978277fe1c25dbb
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-17 18:49:42 -0300

    implement `getHobbies`

--- src/repositories/hobby.repository.ts
+++ src/repositories/hobby.repository.ts
@@ -1,4 +1,5 @@
 import { IHobby } from "../models/hobby.model";
+import connection from "../config/db.config.js";
 
 interface IHobbyRepository {
   getHobbies(searchParams: { hobby?: string }): Promise<IHobby[]>;
@@ -7,8 +8,24 @@ interface IHobbyRepository {
 }
 
 class HobbiesRepository implements IHobbyRepository {
-  getHobbies(searchParams: { hobby?: string | undefined }): Promise<IHobby[]> {
-    throw new Error("Method not implemented.");
+  async getHobbies(searchParams: {
+    hobby?: string | undefined;
+  }): Promise<IHobby[]> {
+    let query: string = "SELECT * FROM hobbies";
+
+    if (searchParams?.hobby) {
+      query = query + ` WHERE LOWER(hobby) LIKE '%${searchParams.hobby}%'`;
+    }
+
+    return new Promise((resolve, reject) => {
+      connection.query<IHobby[]>(query, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });
   }
 
   getHobbiesByNameArray(hobbiesArray: string[]): Promise<IHobby[]> {
