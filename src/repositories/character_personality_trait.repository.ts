2daa41b9997b7bfaca16d46966b975b938ff606d
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-17 18:02:18 -0300

    implements `getAll`

--- src/repositories/character_personality_trait.repository.ts
+++ src/repositories/character_personality_trait.repository.ts
@@ -1,4 +1,5 @@
 import { ICharacterPersonalityTrait } from "../models/character_personality_trait.model";
+import connection from "../config/db.config.js";
 
 interface ICharacterPersonalityTraitRepository {
   getCharacterPersonalityTraits(searchParams: {
@@ -15,7 +16,36 @@ class CharacterPersonalityTraitRepository
     character_id?: string;
     personality_trait_id?: string;
   }): Promise<ICharacterPersonalityTrait[]> {
-    throw new Error("Method not implemented.");
+    let query: string = "SELECT * FROM character_personality_traits";
+    let condition: string[] = [];
+
+    if (searchParams?.character_id) {
+      condition.push(`character_id = '${searchParams.character_id}'`);
+    }
+
+    if (searchParams?.personality_trait_id) {
+      condition.push(
+        `personality_trait_id = '${searchParams.personality_trait_id}'`
+      );
+    }
+
+    for (let i = 0; i < condition.length; i++) {
+      if (i === 0) {
+        query += ` WHERE ${condition[i]}`;
+      } else {
+        query += ` AND ${condition[i]}`;
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      connection.query<ICharacterPersonalityTrait[]>(query, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });
   }
 
   async createCharacterPersonalityTrait(): Promise<ICharacterPersonalityTrait> {
