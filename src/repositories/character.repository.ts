a70daf15313f3a37d6c7d9416489929e1dda5527
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-17 14:46:13 -0300

    implement `createCharacter`

--- src/repositories/character.repository.ts
+++ src/repositories/character.repository.ts
@@ -1,6 +1,7 @@
 import { Character } from "character-forge";
 import connection from "../config/db.config.js";
 import { ICharacter } from "../models/character.model.js";
+import { ResultSetHeader } from "mysql2";
 
 interface ICharacterRepository {
   getCharacters(searchParams: {
@@ -10,6 +11,7 @@ interface ICharacterRepository {
     maxHeight?: number;
   }): Promise<ICharacter[]>;
   getCharacter(id: number, uuid?: string): Promise<ICharacter>;
+  createCharacter(character: ICharacter): Promise<ICharacter>;
 }
 
 class CharacterRepository implements ICharacterRepository {
@@ -66,14 +68,46 @@ class CharacterRepository implements ICharacterRepository {
     }
 
     return new Promise((resolve, reject) => {
-      connection.query<ICharacter[]>(
-        query,
-        [id || uuid],
+      connection.query<ICharacter[]>(query, [id || uuid], (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res?.[0]);
+        }
+      });
+    });
+  }
+
+  async createCharacter(character: ICharacter): Promise<ICharacter> {
+    return new Promise((resolve, reject) => {
+      connection.query<ResultSetHeader>(
+        "INSERT INTO characters (uuid, name, nickname, surname, sex, age, weight, height, hair_color, eye_color, hair_style, ethnicity, birthplace, occupation, social_class, political_view, sexuality, alignment, phobia) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        [
+          character.uuid,
+          character.name,
+          character.nickname,
+          character.surname,
+          character.sex,
+          character.age,
+          character.bodyType.weight,
+          character.bodyType.height,
+          character.hairColor,
+          character.eyeColor,
+          character.hairStyle,
+          character.ethnicity,
+          character.birthplace,
+          character.occupation,
+          character.socialClass,
+          character.politicalView,
+          character.sexuality,
+          character.alignment,
+          character.phobia,
+        ],
         (err, res) => {
           if (err) {
             reject(err);
           } else {
-            resolve(res?.[0]);
+            this.getCharacter(res.insertId).then((character) => resolve(character)).catch(reject)
           }
         }
       );
