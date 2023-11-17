59b32ec42a32ba04dd01597e8a42d604f1aac6eb
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-17 15:07:45 -0300

    add sample cronjob

--- src/controllers/character.controller.ts
+++ src/controllers/character.controller.ts
@@ -1,6 +1,19 @@
+import { CronJob } from "cron";
 import { Request, Response } from "express";
 
 export default class CharacterController {
+  cronJob: CronJob;
+
+  constructor() {
+    this.cronJob = new CronJob("* * * * *", () => {
+      console.log("cron job");
+    });
+
+    if (!this.cronJob.running) {
+      this.cronJob.start();
+    }
+  }
+
   async delete(req: Request, res: Response) {}
 
   async get(req: Request, res: Response) {}
