1dc1eb77f78f51e83ecc5aec757b38a2f4b4565c
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-17 15:07:50 -0300

    add routes

--- src/routes/character.routes.ts
+++ src/routes/character.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
+import CharacterController from "../controllers/character.controller";
 
 class CharacterRoutes {
   router = Router();
+  controller = new CharacterController();
 
   constructor() {
     this.initializeRoutes();
   }
 
-  initializeRoutes() {}
+  initializeRoutes() {
+    this.router.get("/", this.controller.getAll);
+    this.router.get("/:id", this.controller.get);
+    this.router.delete("/:id", this.controller.delete);
+  }
 }
 
 export default new CharacterRoutes().router;
