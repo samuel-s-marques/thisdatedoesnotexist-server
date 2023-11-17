d1527f80d68ae16dda56998e0db561b8b3178f10
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-17 13:57:24 -0300

    Update character.controller.ts

---
+++ src/controllers/character.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export default class CharacterController {
+    async create(req: Request, res: Response) {}
+    
+    async delete(req: Request, res: Response) {}
+
+    async get(req: Request, res: Response) {}
+
+    async getAll(req: Request, res: Response) {}
+}
