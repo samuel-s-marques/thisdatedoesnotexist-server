77df5bce8bd7f89ea1b46a5cf048b1a9236906de
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-18 14:12:25 -0300

    Update firestore.config.ts

--- src/config/firestore.config.ts
+++ src/config/firestore.config.ts
@@ -1,14 +1,12 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import admin from "firebase-admin";
+import serviceAccount from "./serviceAccountKey.json";
 
-const firebaseApp = initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+const firebaseApp = admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: serviceAccount.project_id,
+    clientEmail: serviceAccount.client_email,
+    privateKey: serviceAccount.private_key,
+  }),
 });
 
-export default getFirestore(firebaseApp);
+export default admin.firestore();
