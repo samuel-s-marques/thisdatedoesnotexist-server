38ec6ff029c15fa0d785b60afb551fee14805a94
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-20 12:55:45 -0300

    implement `get`

--- src/controllers/hobby.controller.ts
+++ src/controllers/hobby.controller.ts
@@ -11,10 +11,32 @@ export default class HobbyController {
 
       res.status(200).send(hobbies);
     } catch (error) {
-        res.status(500).send({
-            status: "error",
-            message: "Some error occurred while retrieving hobbies.",
+      res.status(500).send({
+        status: "error",
+        message: "Some error occurred while retrieving hobbies.",
+      });
+    }
+  }
+
+  async get(req: Request, res: Response) {
+    const id: string = req.params.id;
+
+    try {
+      const hobby = await hobbyRepository.getHobby(parseInt(id));
+
+      if (hobby) {
+        res.status(200).send(hobby);
+      } else {
+        res.status(404).send({
+          status: "error",
+          message: `Hobby with id ${id} not found.`,
         });
+      }
+    } catch (error) {
+      res.status(500).send({
+        status: "error",
+        message: `Some error occurred while retrieving hobby with id ${id}.`,
+      });
     }
   }
 }
