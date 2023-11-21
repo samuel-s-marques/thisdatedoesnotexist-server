ae2c22c86bdac2d03d5bdb3cb760851882cdd27d
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-21 14:45:16 -0300

    add `name` and `type`

--- src/models/hobby.model.ts
+++ src/models/hobby.model.ts
@@ -2,6 +2,6 @@ import { RowDataPacket } from "mysql2";
 
 export interface IHobby extends RowDataPacket {
     id?: number;
-    hobby: string;
-    created_at?: Date;
+    name: string;
+    type: string;
 }
