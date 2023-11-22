0388b83da5e6027192fc0b4d9549d5def1c0e092
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-22 16:37:44 -0300

    Update PronounsController.ts

--- app/Controllers/Http/PronounsController.ts
+++ app/Controllers/Http/PronounsController.ts
@@ -1,5 +1,25 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import PronounsModel from 'App/Models/PronounsModel'
 
 export default class PronounsController {
-  public async index(ctx: HttpContextContract) {}
+  public async index(ctx: HttpContextContract) {
+    const page = ctx.request.input('page', 1)
+    const query = ctx.request.input('query')
+    const value = ctx.request.input('value')
+
+    if (query && value) {
+      const pronouns = await PronounsModel.query().where(query, value).paginate(page, 40)
+
+      return pronouns
+    }
+
+    const pronouns = await PronounsModel.query().paginate(page, 40)
+    return pronouns
+  }
+
+  public async show(ctx: HttpContextContract) {
+    const pronoun = await PronounsModel.query().where('id', ctx.params.id).firstOrFail()
+
+    return pronoun
+  }
 }
