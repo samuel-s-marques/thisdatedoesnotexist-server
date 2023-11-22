9f56c7ccd640e66e1595915514330373a2a2207a
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-22 16:38:20 -0300

    Create firebase_database.ts

---
+++ config/firebase_database.ts
@@ -0,0 +1,12 @@
+import admin from 'firebase-admin'
+import serviceAccount from './serviceAccountKey.json'
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: serviceAccount.project_id,
+    clientEmail: serviceAccount.client_email,
+    privateKey: serviceAccount.private_key,
+  }),
+})
+
+export default admin.firestore()
