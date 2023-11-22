35c9cf940314307500168aebe0d98a96b5291e1b
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-22 16:40:29 -0300

    Create routes.ts

---
+++ start/routes.ts
@@ -0,0 +1,44 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer'
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.group(() => {
+    Route.get('/', 'CharactersController.index')
+    Route.get('/:uuid', 'CharactersController.show')
+  }).prefix('/characters')
+
+  Route.group(() => {
+    Route.post('/', 'FeedbacksController.store')
+    Route.get('/', 'FeedbacksController.index')
+    Route.get('/:id', 'FeedbacksController.show')
+  }).prefix('/feedbacks')
+
+  Route.group(() => {
+    Route.get('/', 'HobbiesController.index')
+    Route.get('/:id', 'HobbiesController.show')
+  }).prefix('/hobbies')
+
+  Route.group(() => {
+    Route.get('/', 'PersonalityTraitsController.index')
+    Route.get('/:id', 'PersonalityTraitsController.show')
+  }).prefix('/traits')
+}).prefix('/api')
