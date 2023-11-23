fce328550386b5cbc57de0a8f0d33cb749e6dcf9
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-23 18:32:10 -0300

    Create 1700773191926_body_types.ts

---
+++ database/migrations/1700773191926_body_types.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'body_types'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('sex')
+      table.string('name')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamps(true, true)
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
