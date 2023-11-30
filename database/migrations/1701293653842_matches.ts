9a26b1a62d0084be7b386f2d9f244e7f19c2b524
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-30 15:19:18 -0300

    Change data types of user_id and character_id columns

--- database/migrations/1701293653842_matches.ts
+++ database/migrations/1701293653842_matches.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('user_id')
-      table.integer('character_id')
+      table.string('user_id')
+      table.string('character_id')
 
       table.timestamps(true, true)
     })
