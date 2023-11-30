c622e88201a9fc3f71d69b90c2720c4e069fe283
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-30 15:09:06 -0300

    Add migration for swipes table

---
+++ database/migrations/1701290070963_swipes.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'swipes'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('target_id')
+      table.integer('swiper_id')
+      table.enum('direction', ['left', 'right']).notNullable()
+
+      table.timestamps(true, true)
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
