21c00fe4d23019de8f1264842573c27b4969c2e8
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-12 15:27:05 -0300

    Add Chat model with associations

---
+++ app/Models/Chat.ts
@@ -0,0 +1,24 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, HasMany, belongsTo, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+import Message from './Message'
+
+export default class Chat extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column({ columnName: 'user_id'})
+  public userId: number
+
+  @belongsTo(() => User, { foreignKey: 'user_id' })
+  public user: BelongsTo<typeof User>
+
+  @hasMany(() => Message, { foreignKey: 'chat_id' })
+  public messages: HasMany<typeof Message>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
