502e2c68cbe7adec9fcb93f6c53f6210a1095998
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-12 14:59:19 -0300

    Refactor matches migration to use integer foreign keys and add unique constraint

--- database/migrations/1701293653842_matches.ts
+++ database/migrations/1701293653842_matches.ts
@@ -6,8 +6,9 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('user_id')
-      table.string('character_id')
+      table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
+      table.integer('character_id').unsigned().references('users.id').onDelete('CASCADE')
+      table.unique(['user_id', 'character_id'])
 
       table.timestamps(true, true)
     })
