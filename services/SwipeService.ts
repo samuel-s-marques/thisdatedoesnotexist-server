a251ef49ac2fb3d2828795318cc2ccceba86e2a3
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-14 16:13:38 -0300

    Add SwipeService class for checking swipes

---
+++ services/SwipeService.ts
@@ -0,0 +1,51 @@
+import { Timestamp } from 'firebase-admin/firestore'
+import Logger from '@ioc:Adonis/Core/Logger'
+import User from 'App/Models/User'
+
+class SwipeService {
+  private static instance: SwipeService
+
+  public static getInstance(): SwipeService {
+    if (!SwipeService.instance) {
+      SwipeService.instance = new SwipeService()
+    }
+
+    return SwipeService.instance
+  }
+
+  public async checkSwipes() {
+    try {
+      const now = Timestamp.now()
+      // TODO: Change this to 24 hours
+      let time = now.toMillis() - 5 * 60 * 1000
+      const users = await User.query().where('type', 'user').andWhere('last_swipe', '<', new Date(time))
+
+      if (users.length === 0) {
+        Logger.info('No users with swipes under 20 found.')
+        return
+      }
+
+      for (const user of users) {
+        Logger.warn(`User ${user.uid} has swipes under 20.`)
+
+        user.availableSwipes = 20
+        user.lastSwipe = null
+        await user.save()
+      }
+
+      await Promise.all(
+        users.map(async (user) => {
+          user.availableSwipes = 20
+          user.lastSwipe = null
+          await user.save()
+        })
+      )
+
+      Logger.info('Swipes updated successfully.')
+    } catch (error) {
+      Logger.error('Error checking swipes: ', error)
+    }
+  }
+}
+
+export default SwipeService
