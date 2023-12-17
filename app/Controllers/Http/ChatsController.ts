0a90c99987968446e958535f3f0339659a6a0a5a
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-17 12:07:48 -0300

    Add ChatsController with index method

---
+++ app/Controllers/Http/ChatsController.ts
@@ -0,0 +1,24 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Chat from 'App/Models/Chat'
+import User from 'App/Models/User'
+
+export default class ChatsController {
+  public async index({ request, response }: HttpContextContract) {
+    try {
+      const page = request.input('page', 1)
+      const searchQuery = request.qs()
+      const uid = searchQuery.uid
+
+      if (!uid) {
+        return response.status(400).json({ error: 'User ID is required.' })
+      }
+
+      const user = await User.query().where('uid', uid).firstOrFail()
+      const chats = await Chat.query().where('user_id', user.id).paginate(page, 40)
+
+      return chats
+    } catch (error) {
+      return response.status(400).json({ error: error.message })
+    }
+  }
+}
