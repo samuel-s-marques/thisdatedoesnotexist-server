1fcb3bd66961f7725bd85438bf9bda8d8efcad54
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-18 13:52:52 -0300

    Add socket.ts file for WebSocket functionality

---
+++ start/socket.ts
@@ -0,0 +1,13 @@
+import WsService from 'Service/WsService'
+import { v4 as uuidv4 } from 'uuid'
+WsService.boot()
+
+WsService.wss.on('connection', (ws) => {
+  ws.id = uuidv4()
+  ws.send('Hello from AdonisJS')
+
+  ws.on('message', (data, isBinary) => {
+    const message = isBinary ? data : data.toString()
+    console.log(message)
+  })
+})
