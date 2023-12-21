f877f9dc9fc046b909c5fca56483b8f899f2c96a
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-21 01:02:07 -0300

    Refactor KoboldService to separate sendPrompt and sendMessage methods

--- services/KoboldService.ts
+++ services/KoboldService.ts
@@ -16,19 +16,9 @@ export default class KoboldService {
     return KoboldService.instance
   }
 
-  public async sendMessage(message: string, character: User, user: User, instruction: string) {
+  public async sendPrompt(prompt: string, stop_sequence: string[]) {
     try {
-      Logger.info('Sending message to Kobold AI.')
-      const instructions = instructionsJson[instruction]
-
-      const adaptedMessage = message
-        .replace(/\[input_sequence\]/g, instructions.input_sequence)
-        .replace(/\[output_sequence\]/g, instructions.output_sequence)
-        .replace(/\[separator_sequence\]/g, instructions.separator_sequence)
-      const prompt = `${instructions.input_sequence}Write ${character.name} ${character.surname}'s next reply in this fictional roleplay with ${user.name} ${user.surname}.\n${adaptedMessage}`
-
-      console.log(prompt)
-      // console.log(prompt)
+      Logger.info('Sending prompt to Kobold AI.')
 
       const requestOptions: AxiosRequestConfig = {
         method: 'POST',
@@ -59,17 +49,7 @@ export default class KoboldService {
           sampler_full_determinism: false,
           frmttriminc: false,
           frmtrmblln: false,
-          stop_sequence: [
-            `\n${user.name}:`,
-            `\n${user.name} ${user.surname}:`,
-            '\nYou:',
-            '\n\n',
-            `\n${character.name}:`,
-            `\n${character.name} ${character.surname}:`,
-            instructions.input_sequence,
-            instructions.output_sequence,
-            instructions.separator_sequence,
-          ],
+          stop_sequence: stop_sequence,
         },
       }
 
@@ -77,7 +57,57 @@ export default class KoboldService {
       Logger.info('Got response from Kobold AI.')
       return response.data.results[0].text
     } catch (error) {
-      Logger.error('Error sending message to Kobold AI: ', error)
+      Logger.error('Error sending prompt to Kobold AI: ', error)
     }
+  }
+
+  public async sendMessage(message: string, character: User, user: User, instruction: string) {
+    const instructions = instructionsJson[instruction]
+
+    const adaptedMessage = message
+      .replace(/\[input_sequence\]/g, instructions.input_sequence)
+      .replace(/\[output_sequence\]/g, instructions.output_sequence)
+      .replace(/\[separator_sequence\]/g, instructions.separator_sequence)
+    const prompt = `${instructions.input_sequence}Write ${character.name} ${character.surname}'s next reply in this fictional roleplay with ${user.name} ${user.surname}.\n${adaptedMessage}`
+
+    return this.sendPrompt(prompt, [
+      `\n${user.name}:`,
+      `\n${user.name} ${user.surname}:`,
+      `\n${character.name}:`,
+      `\n${character.name} ${character.surname}:`,
+      instructions.input_sequence,
+      instructions.output_sequence,
+      instructions.separator_sequence,
+    ])
+  }
+
+  public async generateBio(
+    character: User,
+    instruction: string,
+    hobbies: string[],
+    personality_traits: string[]
+  ) {
+    const instructions = instructionsJson[instruction]
+    const prompt = `${
+      instructions.input_sequence
+    }Write Assistant's next reply in this fictional roleplay with User.\nYou're a helpful assistant and straight to the point. Don't be afraid to offend or use NSFW. You can optionally use names.\n${
+      instructions.output_sequence
+    }${instructions.separator_sequence}${
+      instructions.input_sequence
+    } User: Generate a first-person short Tinder bio for a ${character.sex} ${
+      character.occupation
+    } who is ${personality_traits.formattedJoin()} and loves ${hobbies.formattedJoin()}. Name is ${
+      character.name
+    } ${character.surname}. ${instructions.output_sequence}${
+      instructions.separator_sequence
+    }Assistant:`
+
+    return this.sendPrompt(prompt, [
+      `\nUser:`,
+      `\nAssistant:`,
+      instructions.input_sequence,
+      instructions.output_sequence,
+      instructions.separator_sequence,
+    ])
   }
 }
