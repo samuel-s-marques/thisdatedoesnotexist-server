dfae847fe3180a54ca4e9cb6cc1644603bfe74a2
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-25 15:31:26 -0300

    Add Notification model

---
+++ app/Models/Notification.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+
+export default class Notification extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public subtitle: string
+
+  @column()
+  public image: string
+
+  @column({ columnName: 'user_id' })
+  public user_id: number
+
+  @belongsTo(() => User, { foreignKey: 'user_id' })
+  public user: BelongsTo<typeof User>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
