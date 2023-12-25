a8f3a8b50d19ce4c4a2e64be08062f9a8da2efb9
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-25 15:43:08 -0300

    Add index method to NotificationsController

--- app/Controllers/Http/NotificationsController.ts
+++ app/Controllers/Http/NotificationsController.ts
@@ -1,3 +1,26 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
-export default class NotificationsController {}
+export default class NotificationsController {
+  public async index({ request, response }: HttpContextContract) {
+    try {
+      const searchQuery = request.qs()
+      const uid = searchQuery.uid
+
+      if (!uid) {
+        return response.status(400).json({ error: 'User UID is required.' })
+      }
+
+      const user = await User.findByOrFail('uid', uid)
+      const notifications = await user
+        .related('notifications')
+        .query()
+        .orderBy('created_at', 'desc')
+        .paginate(40)
+
+      return notifications
+    } catch (error) {
+      return response.status(400).json({ error: error.message })
+    }
+  }
+}
