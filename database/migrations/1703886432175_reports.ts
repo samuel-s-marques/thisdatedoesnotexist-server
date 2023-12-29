2efc08e0a549cc5050704c71cc69bbc5a308d6dc
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-29 19:59:12 -0300

    Create 1703886432175_reports.ts

---
+++ database/migrations/1703886432175_reports.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'reports'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.enum('type', ['inappropriate content', 'bug', 'other'])
+      table.enum('status', ['pending', 'resolved', 'rejected'])
+      table.text('description').nullable()
+      table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
+      table.integer('character_id').unsigned().references('characters.id').onDelete('CASCADE')
+
+      table.timestamps(true, true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
