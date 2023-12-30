53aeb1fb1a2269578a009e0b21a5fd2a891ce7c3
Samuel Marques <samuel.s.marques@protonmail.com>
2023-12-30 16:06:17 -0300

    Add report validation and character status update

--- app/Controllers/Http/ReportsController.ts
+++ app/Controllers/Http/ReportsController.ts
@@ -1,6 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Report from 'App/Models/Report'
 import User from 'App/Models/User'
+import { findMostCommonString } from 'Util/util'
 
 export default class ReportsController {
   public async index({ request, response }: HttpContextContract) {
@@ -31,13 +32,45 @@ export default class ReportsController {
 
       const user = await User.findByOrFail('uid', user_uid)
       const character = await User.findByOrFail('uid', character_uid)
+
+      const existingReport = await Report.query()
+        .where('user_id', user.id)
+        .andWhere('character_id', character.id)
+        .first()
+
+      if (existingReport) {
+        return response.status(400).json({ error: 'Report already exists.' })
+      }
+
       const report = new Report()
       report.type = type
       report.description = description
       await report.related('user').associate(user)
       await report.related('character').associate(character)
-
       await report.save()
+
+      character.reportsCount++
+      await character.save()
+
+      if (character.reportsCount >= 10 && character.status !== 'suspended') {
+        const reports = await Report.query().where('character_id', character.id)
+        const types = reports.map((report) => report.type)
+        const commonReason = findMostCommonString(types)
+
+        character.status = 'suspended'
+        character.statusReason = commonReason!
+        await character.save()
+      }
+
+      if (character.reportsCount >= 20 && character.status !== 'banned') {
+        const reports = await Report.query().where('character_id', character.id)
+        const types = reports.map((report) => report.type)
+        const commonReason = findMostCommonString(types)
+
+        character.status = 'banned'
+        character.statusReason = commonReason!
+        await character.save()
+      }
 
       return report
     } catch (error) {
