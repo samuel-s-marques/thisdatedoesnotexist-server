59c080768d3d6264d4308e2bb8a038cb65dd8615
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-03 16:10:31 -0300

    Refactor PronounsController to improve caching logic

--- app/Controllers/Http/PronounsController.ts
+++ app/Controllers/Http/PronounsController.ts
@@ -5,14 +5,13 @@ import CacheService from 'Service/CacheService'
 export default class PronounsController {
   public async index(ctx: HttpContextContract) {
     const cache = CacheService.getInstance()
+    if (cache.get('pronouns')) {
+      return cache.get('pronouns')
+    }
 
     const page = ctx.request.input('page', 1)
     const query = ctx.request.input('query')
     const value = ctx.request.input('value')
-
-    if (cache.get('pronouns')) {
-      return cache.get('pronouns')
-    }
 
     if (query && value) {
       const pronouns = await PronounsModel.query().where(query, value).paginate(page, 40)
