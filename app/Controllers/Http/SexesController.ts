e76037220b4aeea448d5a1f37c7869d719bd6570
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-03 16:08:08 -0300

    Add caching for SexesController index method

--- app/Controllers/Http/SexesController.ts
+++ app/Controllers/Http/SexesController.ts
@@ -1,11 +1,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Sex from 'App/Models/Sex'
+import CacheService from 'Service/CacheService'
 
 export default class SexesController {
   public async index(ctx: HttpContextContract) {
+    const cache = CacheService.getInstance()
+    if (cache.get('sexes')) {
+      return cache.get('sexes')
+    }
+    
     const page = ctx.request.input('page', 1)
-
     const sexes = await Sex.query().paginate(page, 70)
+    cache.set('sexes', sexes)
+    
     return sexes
   }
 
