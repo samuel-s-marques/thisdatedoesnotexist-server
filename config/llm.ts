351854dbe4890c42e9caccd0dc953d741e768683
Samuel Marques <samuel.s.marques@protonmail.com>
2024-01-06 14:49:32 -0300

    Add 'api' property to LlmConfig

--- config/llm.ts
+++ config/llm.ts
@@ -1,3 +1,14 @@
-export type LlmConfig = {
-    modelInstructions: string
+type LlmConfig = {
+  modelInstructions: string
+  api: string
+}
+
+/*
+|--------------------------------------------------------------------------
+| Large Language Model Configuration
+|--------------------------------------------------------------------------
+*/
+const llm: LlmConfig = {
+  modelInstructions: 'chatml',
+  api: 'kobold',
 }
