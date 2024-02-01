7b765a44f0fe4baddb7acdd495d7f9c891de2f71
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-01 18:31:17 -0300

    Remove unused code in MessagesController

--- app/Controllers/Http/MessagesController.ts
+++ app/Controllers/Http/MessagesController.ts
@@ -2,8 +2,6 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 import Message from 'App/Models/Message'
 import Chat from 'App/Models/Chat'
-import fs from 'fs'
-import Config from '@ioc:Adonis/Core/Config'
 
 export default class MessagesController {
   public async index({ request, response }: HttpContextContract) {
@@ -25,22 +23,6 @@ export default class MessagesController {
         .paginate(searchQuery.page ?? 1, 40)
 
       return messages
-    } catch (error) {
-      return response.status(400).json({ error: error.message })
-    }
-  }
-
-  public async status({ response }: HttpContextContract) {
-    try {
-      const modelName = Config.get('app.whisper.model')
-      const whisperEnabled = Config.get('app.whisper.enabled')
-      const whisperModelExists = fs.existsSync(`whisper\\models\\ggml-${modelName}.bin`)
-
-      const canSendAudio = whisperEnabled && whisperModelExists
-
-      return response.status(200).json({
-        audio: canSendAudio,
-      })
     } catch (error) {
       return response.status(400).json({ error: error.message })
     }
