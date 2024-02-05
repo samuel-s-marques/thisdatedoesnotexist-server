ae3bd03b812cb6292f6b7a96ffd2654f8e98979d
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-05 17:36:56 -0300

    Refactor error handling in FirebaseAuth middleware

--- app/Middleware/FirebaseAuth.ts
+++ app/Middleware/FirebaseAuth.ts
@@ -6,24 +6,48 @@ export default class FirebaseAuth {
     const token = request.header('Authorization')
 
     if (!token) {
-      return response.unauthorized({ error: 'No token provided' })
+      return response.unauthorized({
+        error: {
+          code: 401,
+          message: 'Unauthorized',
+          details: `No token provided.`,
+        },
+      })
     }
 
     if (!token.startsWith('Bearer ')) {
-      return response.unauthorized({ error: 'Invalid token' })
+      return response.unauthorized({
+        error: {
+          code: 401,
+          message: 'Unauthorized',
+          details: `Invalid token.`,
+        },
+      })
     }
 
     try {
       const decodedToken = await admin.auth().verifyIdToken(token.split(' ')[1])
 
       if (!decodedToken) {
-        return response.unauthorized({ error: 'Invalid token' })
+        return response.unauthorized({
+          error: {
+            code: 401,
+            message: 'Unauthorized',
+            details: `Invalid token.`,
+          },
+        })
       }
 
       request.token = decodedToken
       await next()
     } catch (error) {
-      return response.unauthorized({ error: 'Invalid credentials', message: error })
+      return response.unauthorized({
+        error: {
+          code: 401,
+          message: 'Unauthorized',
+          details: `Invalid credentials: ${error.message}`,
+        },
+      })
     }
   }
 }
