e7a1dc3ab141d04670d0c3028ac73d7c1a6d5caa
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-06 14:11:34 -0300

    Update HobbiesController.ts

--- app/Controllers/Http/HobbiesController.ts
+++ app/Controllers/Http/HobbiesController.ts
@@ -11,16 +11,7 @@ export default class HobbiesController {
     }
 
     const page = ctx.request.input('page', 1)
-    const query = ctx.request.input('query')
-    const value = ctx.request.input('value')
-
-    if (query && value) {
-      const hobbies = await HobbyModel.query().where(query, value).paginate(page, 70)
-
-      return hobbies
-    }
-
-    const hobbies = await HobbyModel.query().paginate(page, 70)
+    const hobbies = await HobbyModel.query().paginate(page, 120)
     cache.set('hobbies', hobbies)
 
     return hobbies
