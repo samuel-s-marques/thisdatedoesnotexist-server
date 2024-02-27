03ac77ff2f72e5790554c5fb942e2da3ba757ef7
Samuel Marques <samuel.s.marques@protonmail.com>
2024-02-27 19:14:53 -0300

    Update ComfyUiService with new loaders and sliders

--- services/ComfyUiService.ts
+++ services/ComfyUiService.ts
@@ -5,7 +5,7 @@ import { generateRandomSeed, imagePromptBuilder, negativeImagePromptBuilder } fr
 import { Character } from 'character-forge'
 import fs from 'fs'
 import Application from '@ioc:Adonis/Core/Application'
-import Config from '@ioc:Adonis/Core/Config';
+import Config from '@ioc:Adonis/Core/Config'
 
 export default class ComfyUiService {
   private static instance: ComfyUiService
@@ -151,6 +151,112 @@ export default class ComfyUiService {
               vae_name: 'vae-ft-mse-840000-ema-pruned.safetensors',
             },
             class_type: 'VAELoader',
+          },
+          '39': {
+            inputs: {
+              lora_name: 'weight_slider_v2.safetensors',
+              strength_model: 0.2,
+              strength_clip: 1,
+              model: ['2', 0],
+              clip: ['2', 1],
+            },
+            class_type: 'LoraLoader',
+            _meta: {
+              title: 'Weight Slider',
+            },
+          },
+          '40': {
+            inputs: {
+              lora_name: 'gender_slider_v1.safetensors',
+              strength_model: 0,
+              strength_clip: 1,
+              model: ['39', 0],
+              clip: ['39', 1],
+            },
+            class_type: 'LoraLoader',
+            _meta: {
+              title: 'Gender Slider',
+            },
+          },
+          '41': {
+            inputs: {
+              lora_name: 'muscle_slider_v1.safetensors',
+              strength_model: 0,
+              strength_clip: 1,
+              model: ['40', 0],
+              clip: ['40', 1],
+            },
+            class_type: 'LoraLoader',
+            _meta: {
+              title: 'Muscle Slider',
+            },
+          },
+          '42': {
+            inputs: {
+              lora_name: 'detail_slider_v4.safetensors',
+              strength_model: 1,
+              strength_clip: 1,
+              model: ['41', 0],
+              clip: ['41', 1],
+            },
+            class_type: 'LoraLoader',
+            _meta: {
+              title: 'Detail Slider',
+            },
+          },
+          '44': {
+            inputs: {
+              lora_name: 'skin_tone_slider_v1.safetensors',
+              strength_model: -1,
+              strength_clip: 1,
+              model: ['42', 0],
+              clip: ['42', 1],
+            },
+            class_type: 'LoraLoader',
+            _meta: {
+              title: 'Skin Tone Slider',
+            },
+          },
+          '45': {
+            inputs: {
+              lora_name: 'breastsizeslideroffset.safetensors',
+              strength_model: -1,
+              strength_clip: 1,
+              model: ['44', 0],
+              clip: ['44', 1],
+            },
+            class_type: 'LoraLoader',
+            _meta: {
+              title: 'Breast Size Slider',
+            },
+          },
+          '54': {
+            inputs: {
+              jpeg_artifact_level: 65,
+              noise_level: 8,
+              adjust_brightness: 1,
+              adjust_color: 1,
+              adjust_contrast: 1,
+              seed: -850025818286687,
+              pixels: ['7', 0],
+            },
+            class_type: 'Dequality',
+            _meta: {
+              title: 'Dequality',
+            },
+          },
+          '57': {
+            inputs: {
+              lora_name: 'ReaLora.safetensors',
+              strength_model: 0.7000000000000001,
+              strength_clip: 1,
+              model: ['45', 0],
+              clip: ['45', 1],
+            },
+            class_type: 'LoraLoader',
+            _meta: {
+              title: 'Load LoRA',
+            },
           },
         },
       }
